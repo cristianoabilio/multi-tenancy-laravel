4e6ed6b7ab4a6668a96a199732cb3c198bcfbdfa
Cristiano Abilio <cristianocafr@gmail.com>
2026-05-25 12:27:51 -0300

    fix: set nullable to add a constrained foreign id

--- database/migrations/2026_05_25_151537_add_company_id_to_clients_table.php
+++ database/migrations/2026_05_25_151537_add_company_id_to_clients_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('clients', function (Blueprint $table) {
-            $table->foreignId('company_id')->constrained();
+            $table->foreignId('company_id')->nullable()->constrained();
         });
     }
 
