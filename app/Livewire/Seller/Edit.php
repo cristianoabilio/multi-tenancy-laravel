af683deed37a3418b788ec398808cb9e72b28cf0
Cristiano Abilio <cristianocafr@gmail.com>
2026-05-28 12:04:23 -0300

    feat: added policies to models client and seller to validate authorization

--- app/Livewire/Seller/Edit.php
+++ app/Livewire/Seller/Edit.php
@@ -12,6 +12,7 @@ class Edit extends Component
 
     public function mount(Seller $seller)
     {
+        $this->authorize('createOrUpdate', Seller::class);
         $this->form->setSeller($seller);
     }
 
@@ -22,6 +23,7 @@ class Edit extends Component
 
     public function save()
     {
+        $this->authorize('createOrUpdate', Seller::class);
         $this->form->save();
     }
 }
